82e56f0cec5ec319bf9c3b5122c1c1fcbf974ab4
Asus <vaxa2320@gmail.com>
2024-06-24 16:18:15 +0300

    ter06

--- src/src/Runner_exm4.java
+++ src/src/Runner_exm4.java
@@ -6,10 +6,8 @@ import java.util.List;
 public class Runner_exm4 {
     public static void main(String[] args) {
         List<String> list1 = Arrays.asList("java", "C#", "javaScript", "PHP");
-
         ///////среди всех элементов колекции выввести
         ////максимальное колчичество символов в элементе
-
         int maxLength = 0;
         for (String item : list1) {
             if (item.length() > maxLength) {
@@ -17,7 +15,6 @@ public class Runner_exm4 {
             }
         }
         System.out.println("max length=" + maxLength);
-
         /////java 8
         maxLength = list1.stream()
                 .mapToInt(str -> str.length())
