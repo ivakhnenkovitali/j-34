0776c3eb6b6bd6ed0de7a5af3fe32d1d1444bf2c
Asus <vaxa2320@gmail.com>
2024-11-29 16:15:58 +0300

    ter04

--- src/src/Runner_exm1.java
+++ src/src/Runner_exm1.java
@@ -7,24 +7,18 @@ import java.util.function.Consumer;
 
 public class Runner_exm1 {
     public static void main(String[] args) {
-
         /////функции интерфейсы java 8
         ////для работы с колекциями
-
         List<String> list1 = Arrays.asList("java", "C++", "javaScript", "PHP");
 
         for (String item : list1) {
             System.out.println(item);
         }
-
-
         //// java8
         ////метод forEach() используетсся для перебора элементов колекции и выполнения некторая действия
         ///во время этого пребора
-
         System.out.println("\njava8");
         Consumer<String> consumer = language -> System.out.println(language);
-
         list1.forEach(consumer);
 
         /////лямбда --вырожение допустима применять как в контексте отдельной локальной ( пример выше)переменной так и в контексте
