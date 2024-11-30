6c32620d0c4733885f777ed695bf58c0bda9e872
Asus <vaxa2320@gmail.com>
2024-11-30 16:16:43 +0300

    ter05

--- src/src/Runner_exm2.java
+++ src/src/Runner_exm2.java
@@ -7,11 +7,8 @@ import java.util.List;
 public class Runner_exm2 {
     public static void main(String[] args) {
         List<String> list1 = Arrays.asList("java", "C#", "javaScript", "PHP");
-
         ////реализавать сортировку элемнтов колекции по позростанию
         ////количество символов в каждом из элементов
-
-
         Comparator<String> comparator = new Comparator<String>() {
             @Override
             public int compare(String str1, String str2) {
@@ -22,8 +19,6 @@ public class Runner_exm2 {
         list1.forEach(item -> System.out.println(item));
   //////java 8
         /////сортировака по убыванию
-
-
     list1.sort((str1, str2) -> str2.length() - str1.length() );
         System.out.println("\n");
     list1.forEach(item -> System.out.println(item));
